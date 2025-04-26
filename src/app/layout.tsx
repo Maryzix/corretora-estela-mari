4c9d23d60348074d8d126ba5edb1c4df26931ad9
Mary Lindsen Lasek Luzia de Oliveira <mary.lasek@cyncly.com>
2025-04-26 21:57:28 +0100

    Feat: changing the site name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Estela Mari Empreendimentos",
+  title: "Empreendimentos em Blumenau/SC",
   description: "Blumenau/SC",
 };
 
